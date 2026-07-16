01ec6cc914633e92997fcef7f6476ded883cd845
ᵒʲⁱ <wargative@gmail.com>
2026-07-16 16:19:14 +0700

    Add Stats component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
 import WhyCendex from "@/components/WhyCendex";
 import Assets from "@/components/Assets";
 import Footer from "@/components/Footer";
@@ -9,6 +10,7 @@ export default function HomePage() {
     <>
       <Navbar />
       <Hero />
+      <Stats />
       <WhyCendex />
       <Assets />
       <Footer />
