b5c1a1a342c1128223357202165d79f9c3e1c343
ᵒʲⁱ <wargative@gmail.com>
2026-07-16 14:42:08 +0700

    Add LeaderboardPage component

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,7 @@
+export default function LeaderboardPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-4xl font-bold">Leaderboard</h1>
+    </main>
+  );
+}
